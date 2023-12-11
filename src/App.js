37e2fe8fd1966f42c7f3fe90a23be9011795d70c
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-11 00:10:22 -0800

    latest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 function App() {
   return (
     <div className="App">
-      <h2>This is my new app</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/signupCreative" element={<SignUp2 />}></Route>
+          <Route path="/category" element={<Category />}></Route>
+          <Route path="/talents" element={<Talents />}></Route>
+          <Route path="/registerOption" element={<RegistraionOption />}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import './Navbar.scss'
+import logo from './images/logo.png'
+
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
+
+
+const Navbar = () => {
+
+  const [openNavbar, setOpenNavvbar] = useState(true)
+
+  const toggleNav = () => {
+    setOpenNavvbar(!openNavbar)
+  }
+
+
+
+  return (
+    <div>
+        <div className='navSection'>
+            <Link to={'/'}><img src={logo} alt="" width={200}/></Link>
+
+
+            <div className='navSecTwoDivOne hideSmall'>
+              <p>Home</p>
+              <p>Properties</p>
+              <p>Become an agent</p>
+              <p>Company</p>
+            </div>
+
+            <div className='navSecTwoDivTwo hideSmall'>
+              <p>Login</p>
+              <button>Sign up</button>
+            </div>
+
+            <div className='navIcons'>
+              {openNavbar ? <HiMiniBars3BottomLeft onClick={toggleNav}/> : <IoMdClose onClick={toggleNav}/>}
+            </div>
+
+
+
+            <div className='displaySmall'>
+              <div className='navSecTwoDivOne'>
+                <p>Home</p>
+                <p>Properties</p>
+                <p>Become an agent</p>
+                <p>Company</p>
+              </div>
+
+              <div className='navSecTwoDivTwo'>
+                <p>Login</p>
+                <button>Sign up</button>
+              </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
