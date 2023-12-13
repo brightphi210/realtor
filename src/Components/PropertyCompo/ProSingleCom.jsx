7265cc04396e48eb3d780ebb3eaf8d20c8bd3172
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-13 03:10:28 -0800

    latest

--- src/Components/PropertyCompo/ProSingleCom.jsx
+++ src/Components/PropertyCompo/ProSingleCom.jsx
@@ -119,8 +119,6 @@ const ProSingleCom = () => {
                     <button className='pBtn'>Purchase</button>
                   </div>
                 </div>
-
-
               </div>
             </div>
 
